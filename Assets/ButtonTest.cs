0b6dfdd6e4acf2efc28df063bdabd2a13f7bee40
Zichen Ma <mazichen1984@gmail.com>
2016-07-28 15:26:12 -0400

    new ui crash course

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+    public Slider Slider;
+    
+    public void DoSomething()
+    {
+        Debug.Log(Slider.value.ToString());
+    }
+    public void DoSomethingWithASlider(Slider slider)
+    {
+        Debug.Log(slider.value.ToString());
+    }
+}
